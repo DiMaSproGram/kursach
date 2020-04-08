c00ce1cfc1434544d6bc03d59cff60494b4b08d2
DiMaSproGRAMM <averasok0808@gamil.com>
2020-04-08 18:45:06 +0300

    v1.0

---
+++ target/classes/static/js/account.js
@@ -0,0 +1,8 @@
+$(document).ready(function f() {
+
+    document.getElementById('menuUl').insertAdjacentHTML("beforeend",
+        "        <li class='menu' id='account'>\n" +
+        "                <a class=\"menuButton\" href=\"/account\" title=\"Аккаунт\">Аккаунт</a>\n" +
+        "              </li>\n"
+    );
+})
---
+++ target/classes/static/js/loginbtn.js
@@ -0,0 +1,7 @@
+$(document).ready(function f() {
+    document.getElementById('menuUl').insertAdjacentHTML("beforeend",
+        "      <li class='menu' id='loginbtn'>\n" +
+        "                <a class=\"menuButton\" href=\"/login\" title=\"Войти\">Войти</a>\n" +
+        "            </li>\n"
+    );
+})
